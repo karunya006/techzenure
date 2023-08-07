7f01d535b58881d6d96a074dc05a8718d3a23ae4
karunya006 <141421495+karunya006@users.noreply.github.com>
2023-08-07 20:53:13 +0530

    Add files via upload

---
+++ ArithmetiOperater.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package techzenure;
+
+/**
+ * 
+ */
+public class ArithmetiOperater {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int x=20;
+		int y=3;
+		System.out.println(10%3);
+		System.out.println();
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x/y);
+		System.out.println(10.0f/3);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SequenceOfElements.java
@@ -0,0 +1,16 @@
+package techzenure;
+
+public class SequenceOfElements {
+
+	public static void main(String[] args) {
+		 int t = 1;
+	        for (int i = 1; i <= 15; i++) {
+	            System.out.print(t + " ");
+	            t += 3;
+	        }
+	    }
+	
+
+	}
+
+
